66308c1d43a44abbfaa942fc3898faa088e67b10
xgebi <sarah@sarahgebauer.com>
2018-07-24 07:55:17 +0200

    public path change

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/ext/slothcms/sloth-admin/',
+    assetsPublicPath: '/sloth-admin/',
 
     /**
      * Source Maps
